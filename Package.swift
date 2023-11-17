50c44caf4f624bdb23cc45fb991686db1cd6f4cb
Mo Ahmad <moaahmad@gmail.com>
2023-11-17 11:23:22 +0000

    Point analytics-swift reference to be our forked version to allow for flush fix

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,8 +21,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(
             name: "Segment",
-            url: "https://github.com/segmentio/analytics-swift.git",
-            from: "1.4.7"
+            url: "https://github.com/reveri-health/analytics-swift-flush-fix",
+            branch: "bsneed/operatingMode"
         )
     ],
     targets: [
